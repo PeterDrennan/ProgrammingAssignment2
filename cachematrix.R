de75061835fb2d66079ff1deffd3aff6d94daf1b
PeterDrennan <peterjdrennan@gmail.com>
2016-02-23 22:04:12 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -7,38 +7,37 @@
 
 
 makeCacheMatrix <- function(x = matrix()) {
-+       m <- NULL
-+       set <- function(y) {
-+             x <<- y
-+             m <<- NULL
-+       }
-+       get <- function() x
-+       setinverse <- function(solve) m <<- solve
-+       getinverse <- function() m
-+       list(set = set, get = get,
-+            setinverse = setinverse,
-+            getinverse = getinverse)
-
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(solve) m <<- solve
+      getinverse <- function() m
+      list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
+}
 
 ## CacheSolve calculates the inverse of the matrix from the previous 
 ##function. CacheSolve will get the inverse from the cache if it has
 ##already been solved, and if the matrix is still the same. 
-
 cacheSolve <- function(x, ...) {
-+       m <- x$getinverse()
-+       if(!is.null(m)) {
-+             message("getting cached data")
-+             return(m)
-+       }
-+       data <- x$get()
-+       m <- solve(data, ...)
-+       x$setinverse(m)
-+       m
-+ }
+      m <- x$getinverse()
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setinverse(m)
+      m
+}
 
 ##TEST IT OUT
 z <- diag(8,4)
-> z
+z
      [,1] [,2] [,3] [,4]
 [1,]    8    0    0    0
 [2,]    0    8    0    0
@@ -46,7 +45,7 @@ z <- diag(8,4)
 [4,]    0    0    0    8
 
 CachedMarix <- makeCacheMatrix(z)
-  cacheSolve(CachedMarix)
+ cacheSolve(CachedMarix)
       [,1]  [,2]  [,3]  [,4]
 [1,] 0.125 0.000 0.000 0.000
 [2,] 0.000 0.125 0.000 0.000
@@ -58,13 +57,13 @@ CachedMarix <- makeCacheMatrix(z)
 ##this is the inverse - inverse of 8 is 1/7 or 0.125.
 
 q <-diag(10,3)
-> q
+q
      [,1] [,2] [,3]
 [1,]   10    0    0
 [2,]    0   10    0
 [3,]    0    0   10
-> CachedMarix <- makeCacheMatrix(q)
-> cacheSolve(CachedMarix)
+CachedMarix <- makeCacheMatrix(q)
+cacheSolve(CachedMarix)
      [,1] [,2] [,3]
 [1,]  0.1  0.0  0.0
 [2,]  0.0  0.1  0.0
